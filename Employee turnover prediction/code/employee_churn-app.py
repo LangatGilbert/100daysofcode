191c001a5d5ef1efeab6700db15e328c406879b5
LangatGilbert <gilbert@aims.ac.za>
2021-04-22 17:23:38 +0300

    employee churn prediction

--- Employee turnover prediction/code/employee_churn-app.py
+++ Employee turnover prediction/code/employee_churn-app.py
@@ -3,6 +3,9 @@ import numpy as np
 import pickle 
 import streamlit as st 
 
+from pycaret.classification import load_model, predict_model
+from pycaret.utils import check_metric
+
 #machine learning package
 #from pycaret.classification import *
 
@@ -19,10 +22,28 @@ Data obtained from [Susanli2016 repo](https://github.com/susanli2016/Machine-Lea
 st.sidebar.header('User Input Features')
 
 st.sidebar.markdown("""
-[Example CSV input file](https://github.com/LangatGilbert/100daysofcode/tree/master/Employee%20turnover%20prediction/data/HR.csv)
+[Example CSV input file](https://github.com/LangatGilbert/100daysofcode/blob/master/Employee%20turnover%20prediction/data/example_hr.csv)
 """)
 
 #collect user input feaures into dataframe
-uploaded_file = st.sidebar.file_uploader("Upload the csv")
+uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type =["csv"])
+input_df = pd.read_csv(uploaded_file)
 
+#Displays the user input features
+st.subheader('User Input features')
+
+if uploaded_file is not None:
+    st.write(input_df)
+else :
+    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters(shown below)')
+    st.write(input_df)
+
+#reading the saved classification model
+deployment_28042020 = load_model('../model/employees_churn_model')
+
+#predict the uploaded data
+new_prediction = predict_model(deployment_28042020, data=uploaded_file)
+
+
+check_metric(new_prediction['left'], new_prediction['Label'], metric = 'AUC')
 
